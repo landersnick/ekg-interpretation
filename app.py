41697db316fc2d9e24be469edc962de896ff8c31
Ethan Reddick <ethanreddick55@gmail.com>
2023-11-11 23:15:16 -0500

    Fixed functionality for showing percentage change over time, although the stability algorithm might need some additional tuning.

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+import statistics
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+def calculate_stability(data):
+    # Assuming that lower variability in data indicates higher stability
+    return max(data) - min(data)  # This represents the amplitude
 
 @app.route('/analyze_ecg', methods=['POST'])
 def analyze_ecg():
@@ -17,8 +22,29 @@ def analyze_ecg():
 
     # Perform the analysis
     conditions = perform_analysis(ecg_readings)
+    
+    # Assuming the third line of the CSV contains the previous ECG data
+    previous_ecg_data_line = data.split('\n')[2]
+    previous_ecg_readings = [float(point) for point in previous_ecg_data_line.split(',') if point]
 
-    return jsonify(conditions)
+    current_stability = calculate_stability(ecg_readings)
+    previous_stability = calculate_stability(previous_ecg_readings)
+    
+    # Calculate stability change as a percentage
+    # A lower value indicates improvement
+    stability_change = ((previous_stability - current_stability) / previous_stability) * 100
+
+    # Assuming the second line contains the date after the age
+    patient_info_line = data.split('\n')[1]
+    patient_date = patient_info_line.split(',')[2].strip()
+
+    response_data = {
+        'conditions': conditions,
+        'stability_change': stability_change,
+        'date': patient_date
+    }
+
+    return jsonify(response_data)
 
 def perform_analysis(data):
     # Example: Basic analysis based on heart rate
